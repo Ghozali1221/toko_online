da27bbe6ecb27efa7c9f6f194f8aac0156cf9ffb
ghozali1223 <ghozalipane@gmail.com>
2024-03-27 14:58:38 +0700

    tgl 27-03-2024

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,5 +8,10 @@ use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, ValidatesRequests;
+ use AuthorizesRequests, ValidatesRequests;
+
+ public function index()
+ {
+  return view('pelanggan.layout.index', ['title' => 'Homepage']);
+ }
 }
---
+++ resources/views/pelanggan/layout/index.blade.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <!-- cdnjs.com -->
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+ <title>Belanjamudah.com | {{ $title }} </title>
+
+ <!-- bootstrap -->
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+ <!-- font-awesome -->
+ <link rel="preconnect" href="https://fonts.googleapis.com">
+ <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+ <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+</head>
+</head>
+
+<body>
+
+ <main>
+  <header>
+
+   <!-- start navbar -->
+   <nav class="navbar navbar-expand-lg" style="background-color: #B6FFFA;">
+    <div class="container-fluid">
+     <a class="navbar-brand" href="#">PT.SAMUDERA GROUP TEKNOLOGI INDONESIA</a>
+     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+     </button>
+     <div class="collapse navbar-collapse justify-content-end gap-4" id="navbarSupportedContent">
+      <ul class="navbar-nav gap-4">
+       <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+       </li>
+       <li class="nav-item">
+        <a class="nav-link" href="#">Shop</a>
+       </li>
+       <li class="nav-item">
+        <a class="nav-link" href="#">Contact us</a>
+       </li>
+       <li class="nav-item">
+        <a class="nav-link" href="#">About</a>
+       </li>
+      </ul>
+      <div class="d-flex gap-4 align-items-center">
+       <button class="btn btn-success" type="button"> Login | Register </button>
+       <i class="fa-solid fa-cart-shopping"></i>
+      </div>
+     </div>
+    </div>
+   </nav>
+   <!-- end navbar -->
+
+  </header>
+ </main>
+
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+
+</html>
